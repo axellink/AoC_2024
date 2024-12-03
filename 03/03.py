eae9cb2fefb14fc456d68e8d43de310a432050f9
Axel Vanzaghi <axel.vanzaghi@ovhcloud.com>
2024-12-03 10:47:50 +0100

    Fixing regex for day 3 with explanation

--- 03/03.py
+++ 03/03.py
@@ -12,12 +12,15 @@ print(sum(int(i[0])*int(i[1]) for i in r.findall(memory)))
 
 # second part
 
-# This attempt with regex gives too much mul, so a too high answer, which the function does not give
-# I don't now why and will investigate
-#filter_r = re.compile("don't\(\).*?do\(\)")
-#filtered_mem = filter_r.sub("",memory)
-#print(sum(int(i[0])*int(i[1]) for i in r.findall(filtered_mem)))
+# first option in regex removes don't()...do() blocks
+# second option in regex removes a don't() to EOF block which has no do() in it
+# the question mark in .*? makes the .* to be the shortest instead of the longest match
+filtered_mem = re.sub("don't\(\).*?do\(\)|don't\(\).*$", "", memory)
+print(sum(int(i[0])*int(i[1]) for i in r.findall(filtered_mem)))
 
+# First, I forgot the case of a don't() with no matching do() until end of file
+# so I made this function to be closer to the problem script
+# you see with the prints in the function where I found my problem with the regex solution
 def get_mult_filtered(memory):
     mul_or_dont_re = re.compile("mul\((\d{1,3}),(\d{1,3})\)|don't\(\)")
     do_re = re.compile("do\(\)")
@@ -34,12 +37,14 @@ def get_mult_filtered(memory):
                 else: # this is a mult()
                     res.append(values)
             else: #no match left, end of file
+                print("end of file while enabled")
                 break
         if not enabled: # look for the next do()
             if m := do_re.search(memory,index):
                 index = m.end()
                 enabled = True
             else: # no match left, end of file
+                print("end of file while disabled")
                 break
     return res
 
