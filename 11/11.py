6a64d063390ddc0c9f36edbc31edee2998b48f46
Axel Vanzaghi <axel.vanzaghi@ovhcloud.com>
2024-12-11 15:47:30 +0100

    DAY 11

---
+++ 11/11.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+
+MAX_BLINK = 75
+CACHE = dict()
+with open("11.txt") as f:
+    init = [int(i) for i in f.readline().strip().split()]
+
+def naive(stone, blink=0):
+    if (stone, blink) in CACHE:
+        return CACHE[(stone, blink)]
+    res = 0
+    if blink == MAX_BLINK:
+        res = 1
+    else:
+        s = str(stone)
+        if len(s)%2 == 0:
+            m = len(s)//2
+            res = naive(int(s[:m]), blink+1) + naive(int(s[m:]), blink+1)
+        else:
+            res = naive(1, blink+1) if stone == 0 else naive(stone*2024, blink+1)
+    CACHE[(stone, blink)] = res
+    return res
+
+print(sum(naive(i) for i in init))
